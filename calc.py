d0147139c6e645a0b022e12e6a71fbc5dbd3e446
vamshi_pittala <mudhirajvamshi5@gmail.com>
2023-04-16 01:49:02 +0930

    added new function of multiplication and division of two number

---
+++ calc.py
@@ -0,0 +1,12 @@
+# adding the function for addition to add two number.
+def add(x,y):
+    return x + y
+#adding the function for subtraction of two numbers.
+def sub(x,y):
+    return x - y
+#adding the function for multiplication of two numbers.
+def mul(x,y):
+    return x * y
+#adding the function for division of two numbers.
+def div(x,y):
+    return x / y
